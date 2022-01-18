f408cf2f5328a599f8e5612497f615aee91efa0e
Hossein <haghshenas3c@gmail.com>
2022-01-18 20:03:13 +0330

    Loops

---
+++ Day4/loops.js
@@ -0,0 +1,8 @@
+/* loops */
+
+/* Loops are used in JavaScript to perform repeated tasks based on a condition.
+ Conditions typically return true or false when analysed.
+  A loop will continue running until the defined condition returns false  */
+
+/* There are basically two types of loops in most computer programming languages,
+   namely, entry controlled loops and exit controlled loops. */
