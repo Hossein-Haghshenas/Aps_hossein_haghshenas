b7b97b12286d8ef9202ab68e70ccee50e7b46be5
Hossein <haghshenas3c@gmail.com>
2022-01-19 11:11:53 +0330

    Update loops.js

--- Day4/loops.js
+++ Day4/loops.js
@@ -6,3 +6,16 @@
 
 /* There are basically two types of loops in most computer programming languages,
    namely, entry controlled loops and exit controlled loops. */
+
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+for (let i = 100; i >= 0; i++) {
+  console.log(i);
+}
+for (let i = 50; i >= 0; i += 10) {
+  console.log(i);
+}
