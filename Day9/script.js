f1e1e341fd1d59d301c258a064833b84877cb256
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-01-26 01:04:09 +0330

    Day9 update #1

---
+++ Day9/script.js
@@ -0,0 +1,14 @@
+//1. Write a function that takes an integer minutes and converts it to seconds.
+
+function convert(minutes) {
+  return minutes * 60;
+}
+
+// !!! Test !!! Don't modify codes below this line
+
+// question 1 tests
+
+convert(6);
+convert(4);
+convert(8);
+convert(60);
