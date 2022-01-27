2ee16d0e7998520728016db2f9cd8c78043cca2f
Hossein <haghshenas3c@gmail.com>
2022-01-28 00:02:20 +0330

    question 2 Day10

--- Day10/script.js
+++ Day10/script.js
@@ -4,12 +4,28 @@ function addition(num) {
   return ++num;
 }
 
+//2. Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
 // !!! Test !!! Don't modify codes below this line
 
 // question 1 tests
 
-addition(2);
+/* addition(2);
 addition(-9);
 addition(0);
 addition(999);
-addition(73);
+addition(73); */
+
+// question 2 tests
+
+/* minMax([14, 35, 6, 1, 34, 54]), [1, 54])
+minMax([1.346, 1.6532, 1.8734, 1.8723]), [1.346, 1.8734])
+minMax([0.432, 0.874, 0.523, 0.984, 0.327, 0.2345]), [0.2345, 0.984])
+minMax([13, 72, 98, 43, 24, 65, 31]), [13, 98])
+minMax([-54, -23, -54, -21]), [-54, -21])
+minMax([-0.473, -0.6834, -0.1287, 0.5632]), [-0.6834, 0.5632])
+minMax([0, 0, 0, 0]), [0, 0]) */
