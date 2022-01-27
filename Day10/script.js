bfbdddb7a42a2a0d9667b75d808ed627168aca9e
Hossein <haghshenas3c@gmail.com>
2022-01-27 23:49:35 +0330

    Day10 question 1

---
+++ Day10/script.js
@@ -0,0 +1,15 @@
+//1. Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addition(num) {
+  return ++num;
+}
+
+// !!! Test !!! Don't modify codes below this line
+
+// question 1 tests
+
+addition(2);
+addition(-9);
+addition(0);
+addition(999);
+addition(73);
