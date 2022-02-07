6de2d713d6d7ab39f3b2e482d66b9290028bce60
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-02-07 23:54:48 +0330

    Done practic1

---
+++ Day14/script.js
@@ -0,0 +1,12 @@
+// 1.Power Calculator
+/* Create a function that takes voltage and current and returns the calculated power. */
+
+function circuitPower(voltage, current) {
+  return voltage * current;
+}
+
+//test
+
+circuitPower(110, 3);
+circuitPower(550, 44);
+circuitPower(67, 390);
