6a2ff0b3decdf164c7086c9aa22104248ea80e1a
Hossein <haghshenas3c@gmail.com>
2022-02-21 23:34:37 +0330

    Create delete btns

--- Day18/script.js
+++ Day18/script.js
@@ -1,22 +1,42 @@
 const inputkey = document.getElementById("inputkey");
 const inputvalue = document.getElementById("inputvalue");
 
-const local = document.getElementById("local");
-const session = document.getElementById("session");
-const cookie = document.getElementById("cookie");
+const addLocal = document.getElementById("a-local");
+const addSession = document.getElementById("a-session");
+const addCookie = document.getElementById("a-cookie");
+
+const deleteLocal = document.getElementById("d-local");
+const deleteSession = document.getElementById("d-session");
+const deleteCookie = document.getElementById("d-cookie");
 
 // local storage
 
-local.addEventListener("click", () => {
+addLocal.addEventListener("click", () => {
   localStorage.setItem(inputkey.value, inputvalue.value);
   console.log(localStorage.getItem("name"));
 });
+deleteLocal.addEventListener("click", () => {
+  localStorage.removeItem(inputkey.value);
+  console.log("item deleted");
+});
 
-session.addEventListener("click", () => {
+// session storage
+
+addSession.addEventListener("click", () => {
   sessionStorage.setItem(inputkey.value, inputvalue.value);
   console.log(sessionStorage.getItem("name"));
 });
+deleteSession.addEventListener("click", () => {
+  sessionStorage.removeItem(inputkey.value);
+  console.log("item deleted");
+});
 
-cookie.addEventListener("click", () => {
+// cookie
+
+addCookie.addEventListener("click", () => {
   document.cookie = "name=hassan";
+});
+deleteCookie.addEventListener("click", () => {
+  document.cookie = inputkey.value;
+  console.log("item deleted");
 });
