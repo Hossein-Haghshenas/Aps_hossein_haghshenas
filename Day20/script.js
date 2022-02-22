36ebf29ea5e97982241de1d1dfd79711d9f292d7
Hossein <haghshenas3c@gmail.com>
2022-02-22 23:03:20 +0330

    Whats oop

---
+++ Day20/script.js
@@ -0,0 +1,6 @@
+// Whats OOP ?
+
+/* OOP (Object-Oriented Programming) is an approach in programming in which data is encapsulated
+ within objects and the object itself is operated on, rather than its component parts. */
+
+/* JavaScript is heavily object-oriented. It follows a prototype-based model  */
