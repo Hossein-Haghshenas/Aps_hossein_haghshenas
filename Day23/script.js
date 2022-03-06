bc17d89fcfb3b415f7a50bea1e702fda3da24149
Hossein <haghshenas3c@gmail.com>
2022-03-06 11:00:23 +0330

    get esfahan weather

--- Day23/script.js
+++ Day23/script.js
@@ -1,5 +1,17 @@
 const Joke = require("daddyjoke");
+const weather = require("weather-js");
 
 Joke.getJoke((joke) => {
   console.log(joke);
 });
+
+console.log(weather);
+
+weather.find(
+  { search: "Isfahan, IR", degreeType: "C" },
+  function (err, result) {
+    if (err) console.log(err);
+
+    console.log(JSON.stringify(result, null, 2));
+  }
+);
