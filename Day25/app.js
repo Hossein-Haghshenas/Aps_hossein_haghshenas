fc6f81a1f63718261daf1a5cef5ddb8209c7658b
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-04-19 17:33:59 +0430

    Create first hello world

--- Day25/app.js
+++ Day25/app.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
+
+app.get("/", (req, res) => {
+  res.send("hello world, wellcome mr.hossein");
+});
+app.get("/dog", (req, res) => {
+  res.send("oh... its a dog !");
+});
+
+app.listen(PORT, () => {
+  console.log(`your server is running on port ${PORT}`);
+});
