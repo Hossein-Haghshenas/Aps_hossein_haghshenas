5c3ad7d9973ed26d93403bd44c2f3f6eec07cded
Hossein <haghshenas3c@gmail.com>
2022-04-19 18:49:05 +0430

    Done first practice

--- Day26/app.js
+++ Day26/app.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
+
+app.get("/chocolate", (req, res) => {
+  const { amount } = req.query;
+  res.send(`you wanna ${amount} chocolate`);
+});
+
+app.get("*", (req, res) => {
+  res.send("Page not found !");
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running on port : ${PORT}`);
+});
