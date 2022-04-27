039c5fa17d194b0b2b30d3cb6451c7188d24c85b
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-04-27 19:21:32 +0430

    Create CaughtPokemon component

--- Day27/my-app/src/App.js
+++ Day27/my-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Logo from "./components/Logo";
 import BestPokemon from "./components/BestPokemon";
+import CaughtPokemon from "./components/CaughtPokemon";
 
 function App() {
   /* abilities */
@@ -10,10 +11,13 @@ function App() {
     { ability: "Run-Away", id: 456786578 },
   ];
 
+  const date = new Date().toLocaleDateString();
+
   return (
     <div className="App">
       <Logo appname="Hossein's " />
       <BestPokemon abilities={abilities} />
+      <CaughtPokemon date={date} />
     </div>
   );
 }
