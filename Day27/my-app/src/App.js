b5ed22bbc75795d5fbdc2e99d43e2be2cf96ed7d
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-04-27 18:46:54 +0430

    Create starter template project

---
+++ Day27/my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Logo from "./components/Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <Logo appname="Hossein's " />
+    </div>
+  );
+}
+
+export default App;
